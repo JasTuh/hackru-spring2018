2b6ad608bfb953386bddafe7dd73da48fe4d9ffe
Jason Tuhy <jasontuhy@gmail.com>
2018-04-21 17:23:41 -0400

    Made userLoggedIn non static

--- pages/userLoggedIn.js
+++ pages/userLoggedIn.js
@@ -2,22 +2,31 @@ import React from 'react';
 import Page from '../layouts/main';
 import UserMenu from '../components/UserMenu';
 
-export default props => (
-  <Page>
-    <nav className="navbar navbar-default userNavBar">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <a className="navbar-brand" href="#">Spotify Playlist Manager</a>
-        </div>
-        <ul className="nav navbar-nav">
-          <li className="active"><a href="#">Home</a></li>
-          <li><a href="#">New Playlist</a></li>
-        </ul>
-        <ul className="nav navbar-nav navbar-right">
-          <li><a href="/logout">Log Out</a></li>
-        </ul>
-      </div>
-    </nav>
-    <UserMenu url={props.url} />
-  </Page>
-);
+var makingPlaylist= false; 
+export default class userLoggedIn {
+  
+  render() {
+    return (
+      <Page>
+          <nav className="navbar navbar-default userNavBar">
+            <div className="container-fluid">
+              <div className="navbar-header">
+                <a className="navbar-brand" href="#">Spotify Playlist Manager</a>
+              </div>
+              <ul className="nav navbar-nav">
+                <li className="active"><a href="#">Home</a></li>
+                <li><a href="#">New Playlist</a></li>
+              </ul>
+              <ul className="nav navbar-nav navbar-right">
+                <li><a href="/logout">Log Out</a></li>
+              </ul>
+            </div>
+          </nav>
+          <p>
+            {makingPlaylist}
+          </p>
+          <UserMenu url={props.url} />
+        </Page>
+    ) 
+  }
+}
